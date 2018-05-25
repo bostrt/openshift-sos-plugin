27fda20fe69179c8feb82b76601040c5b1491b41
Takayoshi Kimura <takayoshi@gmail.com>
2018-05-25 16:16:59 +0900

    Use OC_CMD env var to enforce namespace

--- sos.sh
+++ sos.sh
@@ -4,6 +4,7 @@
 RED='\e[31m'
 GREEN='\e[32m'
 RESET='\e[0m'
+OC_CMD="oc -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE"
 
 # precheck
 oc whoami &> /dev/null
@@ -24,9 +25,9 @@ set -x
 # data capture
 oc version &> $DEST/oc-version.txt
 # Without -w, we cannot get full timestamps, so watch it with timeout command
-timeout 15 oc get event -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE -w &> $DEST/oc-get-event.txt
-oc status -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE &> $DEST/oc-status.txt
-oc get project -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE -o ${KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT} &> $DEST/oc-get-project.${KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT}
+timeout 15 $OC_CMD get event -w &> $DEST/oc-get-event.txt
+$OC_CMD status &> $DEST/oc-status.txt
+$OC_CMD get project -o ${KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT} &> $DEST/oc-get-project.${KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT}
 TARGET_OBJECTS="all,ds,pvc,hpa,quota,limits,sa,rolebinding,replicasets"
 if [ "$KUBECTL_PLUGINS_LOCAL_FLAG_INCLUDE_CONFIGMAP" == "true" ]; then
     TARGET_OBJECTS="$TARGET_OBJECTS,cm"
@@ -34,24 +35,26 @@ fi
 if [ "$KUBECTL_PLUGINS_LOCAL_FLAG_INCLUDE_SECRET" == "true" ]; then
     TARGET_OBJECTS="$TARGET_OBJECTS,secret"
 fi
-oc get $TARGET_OBJECTS -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE -o ${KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT} &> $DEST/oc-get-all.${KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT}
-oc get $TARGET_OBJECTS -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE -o wide &> $DEST/oc-get-all.txt
-PODS=$(oc get pod -o name -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE)
+$OC_CMD get $TARGET_OBJECTS -o ${KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT} &> $DEST/oc-get-all.${KUBECTL_PLUGINS_LOCAL_FLAG_OUTPUT}
+$OC_CMD get $TARGET_OBJECTS -o wide &> $DEST/oc-get-all.txt
+PODS=$($OC_CMD get pod -o name)
 for pod in $PODS; do
-  CONTAINERS=$(oc get $pod --template='{{range .spec.containers}}{{.name}} {{end}}' -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE)
+  CONTAINERS=$($OC_CMD get $pod --template='{{range .spec.containers}}{{.name}} {{end}}')
   for c in $CONTAINERS; do
-    oc logs $pod --container=$c --timestamps -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE &> $DEST/${pod//\//-}_${c//\//-}.log
-    oc logs -p $pod --container=$c --timestamps -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE &> $DEST/${pod//\//-}_${c//\//-}.previous.log
+    $OC_CMD logs    $pod --container=$c --timestamps &> $DEST/${pod//\//-}_${c//\//-}.log
+    $OC_CMD logs -p $pod --container=$c --timestamps &> $DEST/${pod//\//-}_${c//\//-}.previous.log
   done
 done
+
+# openshift-infra
 if [ $KUBECTL_PLUGINS_CURRENT_NAMESPACE == "openshift-infra" ]; then
-  for casspod in $(oc get pods -n openshift-infra -o jsonpath='{range .items[*].metadata}{.name}{"\n"}{end}' | grep cassandra); do
-    oc -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE exec $casspod -- nodetool status &> $DEST/status-$casspod.txt
-    oc -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE exec $casspod -- nodetool tpstats &> $DEST/tpstats-$casspod.txt
-    oc -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE exec $casspod -- nodetool proxyhistograms &> $DEST/proxyhistograms-$casspod.txt
-    oc -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE exec $casspod -- nodetool tablestats hawkular_metrics &> $DEST/tablestats-hawkular_metrics-$casspod.txt
-    oc -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE exec $casspod -- nodetool tablehistograms hawkular_metrics data &> $DEST/tablehistograms-hawkular_metrics-data-$casspod.txt
-    oc -n $KUBECTL_PLUGINS_CURRENT_NAMESPACE exec $casspod -- nodetool tablehistograms hawkular_metrics metrics_tags_idx &> $DEST/tablehistograms-hawkular_metrics-metrics_tags_tdx-$casspod.txt
+  for casspod in $($OC_CMD get pods -o jsonpath='{range .items[*].metadata}{.name}{"\n"}{end}' | grep cassandra); do
+    $OC_CMD exec $casspod -- nodetool status &> $DEST/status-$casspod.txt
+    $OC_CMD exec $casspod -- nodetool tpstats &> $DEST/tpstats-$casspod.txt
+    $OC_CMD exec $casspod -- nodetool proxyhistograms &> $DEST/proxyhistograms-$casspod.txt
+    $OC_CMD exec $casspod -- nodetool tablestats hawkular_metrics &> $DEST/tablestats-hawkular_metrics-$casspod.txt
+    $OC_CMD exec $casspod -- nodetool tablehistograms hawkular_metrics data &> $DEST/tablehistograms-hawkular_metrics-data-$casspod.txt
+    $OC_CMD exec $casspod -- nodetool tablehistograms hawkular_metrics metrics_tags_idx &> $DEST/tablehistograms-hawkular_metrics-metrics_tags_tdx-$casspod.txt
   done
   oc get pods --all-namespaces | wc -l &> $DEST/total-number-of-pods.txt
 fi
